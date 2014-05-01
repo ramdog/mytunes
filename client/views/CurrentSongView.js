f29614662b23b7e8dd05b79e47b7928501a28951
Hack Reactor Students <talegram@gmail.com>
2014-05-01 14:19:40 -0700

    Implement view for showing current song at top of player

---
+++ client/views/CurrentSongView.js
@@ -0,0 +1,20 @@
+// CurrentSongView.js
+
+var CurrentSongView = Backbone.View.extend({
+
+  tagName: 'div',
+
+  template: _.template('<h2 class="current-song"><%= artist %> - <%= title %></h2>'),
+
+  // initialize: function() {
+  //   this.model.on('change:currentSong', function(model){
+  //     this.playerView.setSong(model.get('currentSong'));
+  //   }, this);
+  // },
+
+  render: function(song){
+    this.model = song;
+    return this.$el.html(this.template(this.model ? this.model.attributes : ''));
+  }
+
+});
