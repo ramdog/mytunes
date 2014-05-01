d5391f7f7f1536244fe620b9712bdca3ae15b2ae
Hack Reactor Students <talegram@gmail.com>
2014-04-30 22:04:14 -0700

    Implement dequeue, progress on end

--- test/spec/LibraryEntryViewSpec.js
+++ test/spec/LibraryEntryViewSpec.js
@@ -13,19 +13,19 @@ describe('LibraryEntryView', function() {
 
   // This spec passes already, but it's mutually exclusive with the one below.
   // Comment it out when implementing the song queue.
-  it ('plays clicked songs', function(){
+  it ('plays dblclicked songs', function(){
     sinon.spy(SongModel.prototype, 'play');
 
-    view.$el.children().first().click();
+    view.$el.children().first().dblclick();
     expect(model.play).to.have.been.called;
 
     SongModel.prototype.play.restore();
   });
 
-  xit('queues clicked songs', function(){
+  it('queues clicked songs (on button click)', function(){
     sinon.spy(SongModel.prototype, 'enqueue');
 
-    view.$el.children().first().click();
+    view.$('.enqueue').click();
     expect(model.enqueue).to.have.been.called;
 
     SongModel.prototype.enqueue.restore();
